aa185435e76e266254e12943b515fc071db980da
patrickmonster <rsj11201@gmail.com>
2022-06-21 12:24:20 +0900

    로그 클래스 내부화

---
+++ Event/channelCreate.js
@@ -0,0 +1,11 @@
+'use strict';
+const { Channel } = require('discord.js') // 추가 라이브러리
+/**
+* channelCreate.js - 클라이언트 이벤트
+*  create Tue Jun 21 2022 11:49:02 GMT+0900 (대한민국 표준시)
+ * @param { Channel }  channel 
+* @returns 
+*/
+module.exports = function channelCreate( channel ) {
+
+}
--- src/Client.js
+++ src/Client.js
@@ -7,6 +7,29 @@ const path = require("path");
 /* ============================================================================================ */
 const updateCode = require("./Util/updateCode");
 
+
+const levels = {
+    'debug': 0,
+    'warn': 1,
+    'info': 2,
+    'error': 3,
+    'none': 4
+}
+
+let level = 'warn';
+const formattedDate = _ => {
+    const date = new Date();
+    const hours = `00${date.getHours()}`.slice(-2);
+    const mins = `00${date.getMinutes()}`.slice(-2);
+    const seconds = `00${date.getSeconds()}`.slice(-2);
+    return `${hours}:${mins}:${seconds}`;
+}
+const log = lvl => {
+    return (...args) => {
+        if (levels[lvl] >= levels[level])
+            console.log(`${formattedDate()}]${name}`, ...args);
+    }
+}
 class BasicClient extends Client{
     /**
      * 
@@ -15,6 +38,8 @@ class BasicClient extends Client{
     constructor(clientOptions){
         super(clientOptions);
         const _this = this;
+
+        _this._logLevel = clientOptions.logLevel; // 경고래벨
 
         clientOptions = clientOptions || {};
         updateCode( path.join(__dirname, 'base'), false).forEach((value, key)=>_this[key] = value);
@@ -31,6 +56,23 @@ class BasicClient extends Client{
             require('../cli/event');
             console.error("설정이 완료되면, 프로그램을 다시 시작해 주세요.");
         }
+    }
+
+    /**
+     * 
+     */
+    get logger(){
+        const name = this.user?.tag || "BOT";
+        const log = (...args) => {
+            if (4 >= levels[level])
+                console.log(`${formattedDate()}]${name}`, ...args);
+        };
+        log.log = log('none');
+        log.info = log('info');
+        log.warn = log('warn');
+        log.error = log('error');
+        log.debug = log('debug');
+        return log;
     }
 
     getTotalGuild(){
