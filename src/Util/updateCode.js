1b9422f1f8e11d8f2d4d94bcea71b40e7d35a444
patrickmonster <rsj1120@dongguk.ac.kr>
2022-06-21 01:53:13 +0000

    로그 여부 추가

--- src/Util/updateCode.js
+++ src/Util/updateCode.js
@@ -21,26 +21,31 @@ const loadFile = (dir, libs) => {
 	const name = path.basename(dir, '.js');
 	try {
 		const cmd = require(dir);
-		console.log(`[파일관리자]${libs[name] ? '갱신' : '신규'} - ${dir}`);
+		
+		if ( log ) console.log(`[파일관리자]${libs[name] ? '갱신' : '신규'} - ${dir}`);
 		// 캐시가 없는 경우에만 or 개발 모드일 경우 동작
 		if (cli?.mode == "development" && !libs[name]) nocache(dir, (d) => loadFile(d, libs));
 		libs[name] = cmd;
 	}
 	catch (e) {
 		console.error(e);
-		console.log(`[파일관리자] 파일 제거 - ${dir}`);
+		if ( log ) onsole.log(`[파일관리자] 파일 제거 - ${dir}`);
 		try { delete libs[name]; } catch (e) { ; }
 	}
 };
 
+
+let log = true;
 /**
  * 커맨드 관리 모델
  * @param {*} target 탐색위치
  */
- module.exports = function getCommands(target) {
+ module.exports = function getCommands(target, isLog = false) {
 	const libs = {};
 	const commandFolders = fs.readdirSync(target).filter(file => file.endsWith('.js'));
-	console.log(`[파일관리자] 지정경로 - ${target}`);
+	log = isLog; // 로그 기록 여부
+	 
+	if( log ) console.log(`[파일관리자] 지정경로 - ${target}`);
 	for (const file of commandFolders) {loadFile(path.join(target, file), libs);}
 
 	const get = (name) => libs[name] || libs[Object.keys(libs).find(k => (libs[k].name && libs[k].name == name) || (libs[k].aliases && libs[k].aliases.includes(name)))];
