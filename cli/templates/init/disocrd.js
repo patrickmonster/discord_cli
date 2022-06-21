d9c5f23e3f359628fc58cfbfcd4600f5cda6f48c
patrickmonster <rsj1120@dongguk.ac.kr>
2022-06-21 02:34:13 +0000

    템플릿 간소화

--- cli/templates/init/disocrd.js
+++ cli/templates/init/disocrd.js
@@ -1,11 +1,11 @@
 'use strict';
-const { Client, Intents, LoadCommands }= require("@patrickmonster/discord_cli");
+const { Client, Intents, LoadCommands } = require("@patrickmonster/discord_cli");
 const path = require("path");
 
-const package = require("./package.json");
-process.env.DISCORD_TOKEN = process.env.DISCORD_TOKEN || package.token;
+const pkg = require("./package.json");
+process.env.DISCORD_TOKEN = process.env.DISCORD_TOKEN || pkg.token;
 
-const client = Client({
+const client = new Client({
 	intents : [
         Intents.FLAGS.DIRECT_MESSAGES,
         Intents.FLAGS.GUILDS, 
@@ -18,14 +18,8 @@ const client = Client({
     // other services...
 });
 
-// 클라이언트 이벤트
-LoadCommands(path.join(__dirname, "Event")).forEach((func, name) => client.on(name, func));
-
 // 보조 명령리스트 불러오기
 client.eventButton = LoadCommands(path.join(__dirname, "Button"));
-client.eventMessage = LoadCommands(path.join(__dirname, "Message"));
-
 client.eventCommand = LoadCommands(path.join(__dirname, "Command"));
-client.eventAppCommand = LoadCommands(path.join(__dirname, "AppCommand"));
 
 client.login(process.env.DISCORD_TOKEN).catch(client.logger.error);
